1617dc5e0664cc288c066011d99c28cd128d5860
Mateusz <mszczerba86@gmail.com>
2021-10-07 21:32:19 +0200

    Initial commit fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://sleepy-temple-30396.herokuapp.com/v1/';
+  const apiRoot = 'https://sleepy-island-30396.herokuapp.com/v1/';
   const trelloApiRoot = 'https://sleepy-temple-30396.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
